ed75afafda887147e00310aa86edcf1ce6391f2e
Josh <joshuawen97@gmail.com>
2022-11-04 14:44:33 +0700

    subtotal page done w/ react currency format

--- src/reducer.js
+++ src/reducer.js
@@ -9,18 +9,11 @@ export const initialState = {
 			image:
 				'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg',
 		},
-		{
-			id: '12321341',
-			title:
-				'The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback',
-			price: 12.99,
-			rating: 4,
-			image:
-				'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg',
-		},
 	],
 	user: null,
 };
+export const getBasketTotal = (basket) =>
+	basket?.reduce((amount, item) => item.price + amount, 0);
 
 function reducer(state, action) {
 	console.log(action);
